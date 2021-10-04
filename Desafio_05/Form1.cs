e56c4efd521ef8c425529e7acccc498ce401a16d
17rzpio <rodrigo_pioner@yahoo.com.br>
2021-10-04 13:38:37 -0300

    Desafio_05 concluido

---
+++ Desafio_05/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Desafio_05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection con = new MySqlConnection();
+            con.ConnectionString = Properties.Settings.Default.CST;
+            try
+            {
+                con.Open();
+                con.Close();
+                MessageBox.Show("a conexao foi realizada com sucesso");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(String.Format("Falha na conexao:{0}",ex.Message));
+            }
+
+        }
+    }
+}
